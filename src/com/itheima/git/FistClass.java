a363d090eafadcc991d2e5c72f653a66b4745e8b
xlh2969248598 <2969248598@qq.com>
2018-09-23 16:38:55 +0800

    添加工程到git

---
+++ src/com/itheima/git/FistClass.java
@@ -0,0 +1,14 @@
+package com.itheima.git;
+
+public class FistClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
+    }
+}
